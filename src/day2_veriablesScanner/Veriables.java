2838091d1f39c4090bdccd7c7d50f3dc6214f8e5
Furkan Veli İnal <ifurkanveli@gmail.com>
2023-01-17 17:16:14 +0300

    JavaTekrar

---
+++ src/day2_veriablesScanner/Veriables.java
@@ -0,0 +1,23 @@
+package day2_veriablesScanner;
+
+public class Veriables {
+/*
+Java'da kodlarimizi class'larda olustururuz
+Class'lar ihtiyac duyuldugunda obje olusturmak icin de kullanilir
+
+Java'nin entry point'i (baslangic noktasi) main method'dur.
+Variable'lari datalarimizi store(saklamak) icin olustururuz.
+ */
+
+    public static void main(String[] args) {
+        System.out.println("Java Candir.");
+
+                String            ogrenciIsmi            =                  "  Matrix   .";
+        //      Data türü           variable ismi   assigment (atama)       deger
+
+        System.out.println(ogrenciIsmi); // Matrix
+
+        ogrenciIsmi="Furkan Veli İnal";
+        System.out.println(ogrenciIsmi); // Furkan Veli İnal
+    }
+}
